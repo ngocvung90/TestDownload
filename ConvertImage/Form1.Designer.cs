d72eaa5dce70dc75e6f48ce69466bf9ce847fa96
Vung <ngocvung.dt3.k53@gmail.com>
2017-12-01 01:31:48 +0700

    convert to excel base on platform

--- ConvertImage/Form1.Designer.cs
+++ ConvertImage/Form1.Designer.cs
@@ -35,11 +35,13 @@
             this.label2 = new System.Windows.Forms.Label();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.progressConvert = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(96, 161);
+            this.button1.Location = new System.Drawing.Point(35, 162);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(127, 23);
             this.button1.TabIndex = 0;
@@ -92,11 +94,22 @@
             this.progressConvert.Size = new System.Drawing.Size(90, 12);
             this.progressConvert.TabIndex = 5;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(177, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Export";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(282, 225);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.progressConvert);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -119,6 +132,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.ProgressBar progressConvert;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
     }
 }
 
--- ConvertImage/Form1.cs
+++ ConvertImage/Form1.cs
@@ -10,6 +10,9 @@ using System.Windows.Forms;
 using ImageManager;
 using System.IO;
 using System.Threading;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+using ExcelUtil;
 
 namespace ConvertImage
 {
@@ -50,6 +53,171 @@ namespace ConvertImage
                 MessageBox.Show("Invalid Width or Height, check again.");
             }
         }
+
+        string folderpath = "";
+        private void DoExport()
+        {
+            string sunfrogPath = "sunfrog";
+            string[] listFolder = Directory.GetFiles(folderpath, "*.png", SearchOption.AllDirectories);
+            //2. Update to data table to export new Excel
+            DataTable dtShirtsTS = new DataTable();
+            DataTable dtShirtsSunfrog = new DataTable();
+
+            dtShirtsTS.Columns.Add("Front", typeof(string));
+            dtShirtsTS.Columns.Add("Back", typeof(string));
+            dtShirtsTS.Columns.Add("Front Horizontal Align", typeof(string));
+            dtShirtsTS.Columns.Add("Front Vertical Align", typeof(string));
+            dtShirtsTS.Columns.Add("Back Horizontal Align", typeof(string));
+            dtShirtsTS.Columns.Add("Back Vertical Align", typeof(string));
+            dtShirtsTS.Columns.Add("Title", typeof(string));
+            dtShirtsTS.Columns.Add("Description", typeof(string));
+            dtShirtsTS.Columns.Add("Sunfrog Category", typeof(string));
+            dtShirtsTS.Columns.Add("Keywords", typeof(string));
+            dtShirtsTS.Columns.Add("Duration", typeof(int));
+            dtShirtsTS.Columns.Add("Url", typeof(string));
+            dtShirtsTS.Columns.Add("Private", typeof(string));
+            dtShirtsTS.Columns.Add("Auto Restart", typeof(string));
+            dtShirtsTS.Columns.Add("Goal", typeof(int));
+
+            dtShirtsSunfrog.Columns.Add("Front", typeof(string));
+            dtShirtsSunfrog.Columns.Add("Back", typeof(string));
+            dtShirtsSunfrog.Columns.Add("Front Horizontal Align", typeof(string));
+            dtShirtsSunfrog.Columns.Add("Front Vertical Align", typeof(string));
+            dtShirtsSunfrog.Columns.Add("Back Horizontal Align", typeof(string));
+            dtShirtsSunfrog.Columns.Add("Back Vertical Align", typeof(string));
+            dtShirtsSunfrog.Columns.Add("Title", typeof(string));
+            dtShirtsSunfrog.Columns.Add("Description", typeof(string));
+            dtShirtsSunfrog.Columns.Add("Sunfrog Category", typeof(string));
+            dtShirtsSunfrog.Columns.Add("Keywords", typeof(string));
+            dtShirtsSunfrog.Columns.Add("Duration", typeof(int));
+            dtShirtsSunfrog.Columns.Add("Url", typeof(string));
+            dtShirtsSunfrog.Columns.Add("Private", typeof(string));
+            dtShirtsSunfrog.Columns.Add("Auto Restart", typeof(string));
+            dtShirtsSunfrog.Columns.Add("Goal", typeof(int));
+
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            Excel.Range range;
+
+            string str, strSunfrog;
+            int rCnt;
+            int cCnt;
+            int rw = 0;
+            int cl = 0;
+
+            xlApp = new Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Open(folderpath + @"\Import.xlsx", 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            range = xlWorkSheet.UsedRange;
+            rw = range.Rows.Count;
+            cl = range.Columns.Count;
+
+            double step = (double)(100.0f / rw);
+            
+            for (rCnt = 2; rCnt <= rw; rCnt++)
+            {
+                List<string> arrValues = new List<string>();
+                DataRow dataRow = dtShirtsTS.NewRow();
+                DataRow dataRowSunFrog = dtShirtsSunfrog.NewRow();
+                int col = 0;
+                for (cCnt = 1; cCnt <= cl; cCnt++)
+                {
+                    if (range.Cells[rCnt, cCnt] != null && range.Cells[rCnt, cCnt].Value != null)
+                    {
+                        str = (range.Cells[rCnt, cCnt] as Excel.Range).Value2.ToString();
+                        strSunfrog = str;
+                        if (cCnt == 1)//path
+                        {
+                            if (!File.Exists(Path.GetDirectoryName(str) + @"\" + sunfrogPath + @"\" + Path.GetFileName(str)))
+                            {
+                                #region SunFrog dimension WxH = 2400 x 3200
+                                IImageInfo imgBaseSunFrog = WebManager.GetImageInfo(File.ReadAllBytes(str));
+                                imgBaseSunFrog.FileName = Path.GetFileName(str);
+                                imgBaseSunFrog.ContentType = Path.GetExtension(str);
+
+                                imgBaseSunFrog.Path = sunfrogPath;
+                                IImageInfo sunFrogImage = imgBaseSunFrog.ResizeMe(3200, 2400);
+                                string newPath = Path.GetDirectoryName(str) + @"\" + imgBaseSunFrog.Path;
+                                if (!Directory.Exists(newPath))
+                                    Directory.CreateDirectory(newPath);
+                                sunFrogImage.Save(newPath);
+                                #endregion
+                            }
+
+                            str = Path.GetDirectoryName(str) + @"\" + sunfrogPath + @"\" + Path.GetFileName(str);
+                            strSunfrog = str;
+                        }
+                        else if (cCnt == 8)//desc culumn
+                            strSunfrog = dataRowSunFrog[6].ToString();
+                        else if(cCnt == 10)//col keyword, sunfrog only allow 3 keyword
+                        {
+                            string[] arrKeywords = str.Split(',');
+                            if(arrKeywords.Length > 3)
+                            {
+                                string newKey = "";
+                                for (int i = 0; i < 3; i++)
+                                {
+                                    newKey += arrKeywords[i];
+                                    if (i != 2) newKey += ",";
+                                }
+                                strSunfrog = newKey;
+                            }
+
+                        }
+                        arrValues.Add(str);
+                        dataRow[col] = str;
+                        dataRowSunFrog[col] = strSunfrog;
+                        col++;
+                    }
+                    else
+                    {
+                        dataRow[col] = "";
+                        dataRowSunFrog[col] = "";
+                        col++;
+                        arrValues.Add("");
+                    }
+                }
+                dtShirtsTS.Rows.Add(dataRow);
+                dtShirtsSunfrog.Rows.Add(dataRowSunFrog);
+
+                //update UI
+                this.BeginInvoke((Action)delegate ()
+                {
+                    double newStep = rCnt * step;
+                    progressConvert.Value = (int)newStep;
+                });
+            }
+
+            ExcelUtlity excelUtil = new ExcelUtlity();
+            excelUtil.WriteDataTableToExcel(dtShirtsSunfrog, "Sheet1", folderpath + @"\ImportSunfrog.xlsx");
+            excelUtil.WriteDataTableToExcel(dtShirtsTS, "Sheet1", folderpath + @"\ImportOther.xlsx");
+            xlWorkBook.Close(true, null, null);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+
+
+            //update UI
+            this.BeginInvoke((Action)delegate ()
+            {
+                MessageBox.Show("Finished ! Check new excel path : " + folderpath + @"\ImportSunfrog.xlsx", "Notice", MessageBoxButtons.OK);
+            });
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            folderBrowserDialog1.ShowDialog();
+            folderpath = folderBrowserDialog1.SelectedPath;
+            progressConvert.Value = 0;
+
+            Thread t = new Thread(new ThreadStart(DoExport));
+            t.Start();
+        }
+
         private void DoConvert() 
         {
             string newPath = "";
